7d916c282ce667d6a073ff12b112986fea7abe51
KangSDhi <kangteknisi@gmail.com>
2021-05-22 03:03:28 +0700

    Styling the navigation bar

--- resources/js/Index.vue
+++ resources/js/Index.vue
@@ -1,7 +1,11 @@
 <template>
     <div>
-        <router-link v-bind:to="{ name: 'home' }">Home</router-link>
-        <router-link :to="{ name: 'second' }">Second</router-link>
-        <router-view></router-view>
+        <nav class="navbar bg-light border-bottom navbar-light">
+            <router-link class="navbar-brand mr-auto" v-bind:to="{ name: 'home' }">Laravue</router-link>
+            <router-link class="btn nav-button" :to="{ name: 'second' }">Second</router-link>
+        </nav>
+        <div class="container mt-4 mb-4 pr-4 pl-4">
+            <router-view></router-view>
+        </div>
     </div>
 </template>
